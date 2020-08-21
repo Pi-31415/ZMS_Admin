059f4e46b1ddf4348b62b7bc3b0ed5bcf1bbbb04
Pi-31415 <paingthetko.pi@gmail.com>
2020-08-21 23:06:33 +0630

    .

--- src/components/table_student.js
+++ src/components/table_student.js
@@ -7,6 +7,7 @@ function Header(props) {
     const { useState } = React;
 
     const [columns, setColumns] = useState([
+        { title: 'ID', field: 'ID' },
         { title: 'Username', field: 'USERNAME' },
         { title: 'First Name', field: 'FIRST_NAME' },
         { title: 'Last Name', field: 'LAST_NAME' },
@@ -49,7 +50,39 @@ function Header(props) {
                     }),
                 onRowUpdate: (newData, oldData) =>
                     new Promise((resolve, reject) => {
-                        console.log(oldData.ID)
+                        console.log(newData.USERNAME)
+
+                        console.log("Old Data", data);
+                        axios.post(`https://zmsedu.com/api/admin/user/edit`, {
+                            ID: newData.ID,
+                            USERNAME: newData.USERNAME,
+                            FIRST_NAME: newData.FIRST_NAME,
+                            LAST_NAME: newData.LAST_NAME,
+                            PHONE: newData.PHONE,
+                            EMAIL: newData.EMAIL,
+                            ROLE: newData.ROLE,
+                        })
+                            .then(res => {
+                                const users = res.data.USERS;
+                                setData({ users });
+                                console.log("New Data", data);
+                                axios.post(`https://zmsedu.com/api/admin/user/get`, {
+                                    ROLE: "Student"
+                                })
+                                    .then(res => {
+                                        const users = res.data.USERS;
+                                        setData({ users });
+                                        console.log(data);
+                                        resolve();
+                                    }).catch(error => {
+                                        console.log(error);
+                                    });
+                            }).catch(error => {
+                                console.log(error);
+                            });
+
+
+
                     }),
                 onRowDelete: oldData =>
                     new Promise((resolve, reject) => {
@@ -76,6 +109,7 @@ function Header(props) {
                             }).catch(error => {
                                 console.log(error);
                             });
+
                     }),
             }}
         />

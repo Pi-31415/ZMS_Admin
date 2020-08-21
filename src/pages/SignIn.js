d4a2b782fff1ebdf7f396ae606d16dfd0f3c846a
Pi-31415 <paingthetko.pi@gmail.com>
2020-08-21 18:59:33 +0630

    .

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import { Link } from 'react-router-dom';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
@@ -46,42 +45,42 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function SignInSide() {
-  const classes = useStyles();  
+  const classes = useStyles();
   let history = useHistory();
 
 
   useEffect(() => {
     console.log(`${username}`);
     localStorage.setItem("Username", `${username}`);
-    localStorage.setItem("UsernameDisplay","");
+    localStorage.setItem("UsernameDisplay", "");
     console.log(`${auth}`);
-    if(`${auth}` === `success`){
+    if (`${auth}` === `success`) {
       document.getElementById("message").innerHTML = `${auth}`;
       history.push("/portal/dashboard/home");
-    }else if(`${auth}` === `not_logged_in`){
+    } else if (`${auth}` === `not_logged_in`) {
       document.getElementById("message").innerHTML = ``;
     }
-    else{
+    else {
       document.getElementById("message").innerHTML = `Wrong Username or Password`;
     }
   });
-  
-   const [username, setUsername] = useState("");
-   const [password, setPassword] = useState("");
-   const [auth,setAuth] = useState("not_logged_in");
 
-   const handleSubmit = (evt) => {
-       evt.preventDefault();
-       const query = {
-        "USERNAME": username,
-        "PASSWORD": password
-        };
-        axios.post('https://zmsedu.com/api/login', query)
-        .then(response => setAuth(response.data.RESULT))
-        .catch(error => {
-          setAuth("Wrong Username or Password");
-        });
-   }
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [auth, setAuth] = useState("not_logged_in");
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    const query = {
+      "USERNAME": username,
+      "PASSWORD": password
+    };
+    axios.post('https://zmsedu.com/api/login', query)
+      .then(response => setAuth(response.data.RESULT))
+      .catch(error => {
+        setAuth("Wrong Username or Password");
+      });
+  }
 
   return (
     <Grid container component="main" className={classes.root}>
@@ -91,7 +90,11 @@ export default function SignInSide() {
         <div className={classes.paper}>
 
           <img src={zmslogo} align="center" alt="logo" width="100"></img>
-          
+          <Box mt={5} align="center">
+            <Typography>
+              <h3>Admin Login</h3>
+            </Typography>
+          </Box>
           <form className={classes.form} noValidate onSubmit={handleSubmit}>
             <TextField
               variant="outlined"
@@ -119,9 +122,9 @@ export default function SignInSide() {
               id="password"
               autoComplete="current-password"
             />
-            
+
             <Button
-              type="submit" 
+              type="submit"
               fullWidth
               variant="contained"
               color="primary"
@@ -130,12 +133,7 @@ export default function SignInSide() {
               Log in
             </Button>
             <Box align="center">
-            <Typography style={{color:'red'}} id="message">
-            </Typography>
-            </Box>
-            <Box mt={5} align="center">
-              <Typography>
-                <Link to="/portal/signup" className="custom-link">{"Don't have an account? Sign Up"}</Link>
+              <Typography style={{ color: 'red' }} id="message">
               </Typography>
             </Box>
             <Box mt={5}>

9a3906b6849d18c324a32f49a65944f77a6dfe41
Pi-31415 <paingthetko.pi@gmail.com>
2020-08-21 18:17:06 +0630

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Refactored_ZMS_Portal
+ Refactored ZMS Portal for hosting directly on site.
+
+
+-Work in Progress
+Upload API
+
+Uses Axios
+Chart JS for Chart
+Moment JS
---
+++ build/precache-manifest.9f0fc6450982c2c38e68f1ea6cf2d7e3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d17578c582c2a2c6d5c57d2fe21e595d",
+    "url": "/portal/index.html"
+  },
+  {
+    "revision": "b15415e3bec2f25b34af",
+    "url": "/portal/static/css/main.18d7077e.chunk.css"
+  },
+  {
+    "revision": "ef897814e6fbb894bf66",
+    "url": "/portal/static/js/2.3de32156.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portal/static/js/2.3de32156.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b15415e3bec2f25b34af",
+    "url": "/portal/static/js/main.b73aaf2d.chunk.js"
+  },
+  {
+    "revision": "9738bd1c940f101cd278",
+    "url": "/portal/static/js/runtime-main.c2144060.js"
+  },
+  {
+    "revision": "2773a39a086cd6657eab133749ca809c",
+    "url": "/portal/static/media/avatar.2773a39a.png"
+  },
+  {
+    "revision": "458ed253558496fcbc67d81a3c9632f5",
+    "url": "/portal/static/media/favicon_source.458ed253.png"
+  }
+]);

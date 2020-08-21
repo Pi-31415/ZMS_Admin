a287703e4fddb314fbeb05426a3a559c4091e02f
Pi-31415 <paingthetko.pi@gmail.com>
2020-08-21 22:03:54 +0630

    .

--- src/components/table_student.js
+++ src/components/table_student.js
@@ -3,20 +3,25 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
 const App = props => {
-    const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState();
     useEffect(() => {
         axios.post('https://zmsedu.com/api/admin/user/get', {
 
         })
             .then(function (response) {
                 console.log(response.data);
+                var result = Object.keys(response.data.USERS).map((key) => [Number(key), response.data.USERS[key]]);
+                setUsers(result);
+                console.log(users);
+
+
             })
             .catch(function (error) {
                 console.log(error);
             });
     }, []);
     const add = () => {
-       
+
     }
     return (<div>
 

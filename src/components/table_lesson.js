38e38f32be76235fbe6a10f5c41a5977dff5710e
Pi-31415 <paingthetko.pi@gmail.com>
2020-08-23 13:47:24 +0630

    .

--- src/components/table_lesson.js
+++ src/components/table_lesson.js
@@ -4,6 +4,35 @@ import axios from 'axios';
 
 function Header(props) {
 
+    const [columns, setColumns] = useState([
+        //Add Columns
+    ]);
+
+    const [data, setData] = useState(
+        {
+            lessons: []
+        }
+
+    );
+
+    const api_get = "https://zmsedu.com/api/admin/user/get";
+    const api_update = "https://zmsedu.com/api/admin/user/edit";
+    const api_delete = "https://zmsedu.com/api/admin/user/delete";
+    const api_add = "https://zmsedu.com/api/admin/user/add";
+
+    useEffect(() => {
+        axios.post(api_get, {
+            //ROLE: "Student"
+        })
+            .then(res => {
+                const lessons = res.data.LESSONS;
+                setData({ lessons });
+                console.log("Adding", data);
+            }).catch(error => {
+                alert(error);
+            });
+    }, []);
+
     return (
         <h1>Hello</h1>
     )

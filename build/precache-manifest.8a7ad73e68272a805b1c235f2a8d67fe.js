0c993e24761c046ad3d994d2cccac05d88df27ae
Pi-31415 <paingthetko.pi@gmail.com>
2020-08-26 22:59:07 +0630

    .

---
+++ build/precache-manifest.8a7ad73e68272a805b1c235f2a8d67fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0d6a7ff6d7c4474beb445ee321fb38e",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "c1f59bf1f17512f9aa56",
+    "url": "/admin/static/css/main.18d7077e.chunk.css"
+  },
+  {
+    "revision": "8b6a85bcca943edf3f81",
+    "url": "/admin/static/js/2.a074537d.chunk.js"
+  },
+  {
+    "revision": "c0dfa228a6318cbb4cc5cc01394bce2c",
+    "url": "/admin/static/js/2.a074537d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1f59bf1f17512f9aa56",
+    "url": "/admin/static/js/main.fd8594ad.chunk.js"
+  },
+  {
+    "revision": "9194c27616b5d26fd5e5",
+    "url": "/admin/static/js/runtime-main.1fac68e9.js"
+  },
+  {
+    "revision": "57a3e864271896a33f76d4b10cad3e89",
+    "url": "/admin/static/media/avatar.57a3e864.png"
+  },
+  {
+    "revision": "458ed253558496fcbc67d81a3c9632f5",
+    "url": "/admin/static/media/favicon_source.458ed253.png"
+  }
+]);

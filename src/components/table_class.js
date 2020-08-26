45bf671706155735e0a1e69e3e7f41eb0b903cd6
Pi-31415 <paingthetko.pi@gmail.com>
2020-08-26 16:59:55 +0630

    .

--- src/components/table_class.js
+++ src/components/table_class.js
@@ -11,7 +11,7 @@ import Container from '@material-ui/core/Container';
 import MDReactComponent from 'markdown-react-js';
 import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
-
+import MaterialTable from 'material-table'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -25,9 +25,27 @@ const useStyles = makeStyles((theme) => ({
 
 var course_apicall = [];
 var class_apicall = [];
+var coursename_lookup = [];
+var courseid_lookup = [];
 var current_course = "";
+var courselookup_main = { '1': 'İstanbul', '2': 'Şanlıurfa' };
+
+const columns = [
+    { title: 'STUDENTS', field: 'STUDENTS' },
+    { title: 'CLASS_ID', field: 'CLASS_ID' },
+    {
+        title: 'COURSE_ID', 
+        field: 'COURSE_ID',
+        lookup: courselookup_main
+    },
+    { title: 'TEACHER', field: 'TEACHER' },
+    { title: "NEXT_DATETIME", field: "NEXT_DATETIME" },
+
+];
 
 class Syllabus extends React.Component {
+
+
 
     constructor(props) {
         super(props);
@@ -52,14 +70,25 @@ class Syllabus extends React.Component {
                     .then(res => {
                         const classes = res.data.CLASS;
                         class_apicall = classes;
-                        this.setState({ CLASS_ARRAY: class_apicall,COURSE_ARRAY: course_apicall });
+                        this.setState({ CLASS_ARRAY: class_apicall, COURSE_ARRAY: course_apicall });
                         console.log(this.state);
+                        //then generate the lookup values for material-table
+                        var i;
+                        for (i = 0; i < this.state.COURSE_ARRAY.length; i++) {
+                            coursename_lookup.push(this.state.COURSE_ARRAY[i].NAME);
+                            courseid_lookup.push(this.state.COURSE_ARRAY[i].ID);
+                            //courselookup_main[i] = [{ '1': 'İstanbul', '2': 'Şanlıurfa' }];
+                            courselookup_main[this.state.COURSE_ARRAY[i].ID] = this.state.COURSE_ARRAY[i].NAME;
+                            //console.log(courselookup_main);
+                        }
+
                     }).catch(error => {
                         alert(error);
                     });
             }).catch(error => {
                 alert(error);
             });
+
     }
 
 
@@ -68,8 +97,16 @@ class Syllabus extends React.Component {
         return (
             <div>
                 <Paper style={{ padding: 20, paddingTop: 30 }}>
-
                 </Paper>
+                <br></br>
+                <div style={{ maxWidth: '100%' }}>
+                    <MaterialTable
+                        columns={columns}
+                        data={this.state.CLASS_ARRAY}
+                        title="Demo Title"
+                    />
+                </div>
+
             </div>
         );
     }

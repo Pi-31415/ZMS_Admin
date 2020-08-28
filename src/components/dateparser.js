0d24bcf9658056cf0ab8c83a163c3ad480d23aee
Pi-31415 <paingthetko.pi@gmail.com>
2020-08-28 22:14:37 +0630

    .

--- src/components/dateparser.js
+++ src/components/dateparser.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import Moment from 'react-moment';
- 
+
+function formatdate(inputdate) {
+    //format the time from server and return the Hong Kong Time zone
+    var res = inputdate.toString().split(".");
+    var moment = require('moment-timezone');
+    var utcCutoff = moment.utc(res[0], '');
+    return utcCutoff.toString();
+  }
+
 function Headline(props) {
-  return <h1>{props.value}</h1>;
+  return <h1>{formatdate(props.value)}</h1>;
 }
  
 export default Headline;

b55df6c724858ff01ea02e72a246b14296a54b5a
Pi-31415 <paingthetko.pi@gmail.com>
2020-08-28 22:12:03 +0630

    .

--- src/components/dateparser.js
+++ src/components/dateparser.js
@@ -1,13 +1,8 @@
 import React from 'react';
  
-function App() {
-  const greeting = 'Hello Function Component!';
- 
-  return <Headline value={greeting} />;
-}
  
 function Headline(props) {
   return <h1>{props.value}</h1>;
 }
  
-export default App;
+export default Headline;

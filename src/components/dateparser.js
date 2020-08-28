5585ef2f2d160b5b23ac623bdb62d292b19f1bae
Pi-31415 <paingthetko.pi@gmail.com>
2020-08-28 21:43:16 +0630

    .

--- src/components/dateparser.js
+++ src/components/dateparser.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
-
-class Copyright extends React.Component {
-    render() {
-        return (
-            <Typography variant="body2" color="textSecondary" align="center">
-                {'Copyright © ZMS Education & Technology '}
-                {new Date().getFullYear()}
-                {'.'}
-            </Typography>
-        );
-    }
+ 
+function App() {
+  const greeting = 'Hello Function Component!';
+ 
+  return <Headline value={greeting} />;
 }
-export default Copyright;
+ 
+function Headline(props) {
+  return <h1>{props.value}</h1>;
+}
+ 
+export default App;

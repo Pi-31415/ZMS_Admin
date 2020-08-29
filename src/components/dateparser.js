e87dd2f11cc4c05e2a6df57d71414cd05283abdb
Pi-31415 <paingthetko.pi@gmail.com>
2020-08-29 14:21:14 +0630

    .

--- src/components/dateparser.js
+++ src/components/dateparser.js
@@ -2,15 +2,17 @@ import React from 'react';
 import Moment from 'react-moment';
 
 function formatdate(inputdate) {
-    //format the time from server and return the Hong Kong Time zone
-    var res = inputdate.toString().split(".");
-    var moment = require('moment-timezone');
-    //var utcCutoff = moment.utc(res[0], '');
-    return res[0].toString();
-  }
+  //format the time from server and return the Hong Kong Time zone
+  var res = inputdate.toString().split(".");
+  var moment = require('moment-timezone');
+  var utcCutoff = moment.utc(res[0], '');
+  var displayCutoff = utcCutoff.clone().tz('Asia/Yangon');
+  localStorage.setItem("tutordate", moment(displayCutoff).format('hh:mm a'));
+  return displayCutoff;
+}
 
 function Headline(props) {
   return <Moment format="DD-MMM-YYYY (ddd) hh:mm a">{formatdate(props.value)}</Moment>;
 }
- 
+
 export default Headline;

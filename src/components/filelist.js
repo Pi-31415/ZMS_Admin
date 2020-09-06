d052684c9b38b19c43ddcc247dc7cfaa941a12ca
Pi-31415 <paingthetko.pi@gmail.com>
2020-09-06 21:27:11 +0630

    .

--- src/components/filelist.js
+++ src/components/filelist.js
@@ -37,12 +37,12 @@ class FileUpload extends React.Component {
     render() {
         return (
             <div>
-                <ul>
+                <ol>
                     {
                         this.state.filelist[0] == undefined ? "No uploaded files." :
-                        this.state.filelist.map((u) => <Individualfile lessonid={this.props.lessonid} homeworkid={this.state.homeworkid} fileid={u.FILE_ID} name={u.NAME}></Individualfile>)
+                        this.state.filelist.map((u) => <li><Individualfile lessonid={this.props.lessonid} homeworkid={this.state.homeworkid} fileid={u.FILE_ID} name={u.NAME}></Individualfile></li>)
                     }
-                </ul>
+                </ol>
             </div>
         )
     }

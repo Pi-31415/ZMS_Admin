c41e684a80b9d286099df2b34b5765e511065875
Pi-31415 <paingthetko.pi@gmail.com>
2020-09-06 19:52:48 +0630

    .

---
+++ src/components/indifile.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import axios from 'axios';
+
+class FileUpload extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            filelist: ''
+        }
+
+    }
+
+    componentDidMount() {
+        
+    }
+
+    render() {
+        return (
+            <div>
+                <ul>
+                    {
+                        this.state.filelist[0] == undefined ? "No uploaded files." :
+                        this.state.filelist.map((u) => <li onClick={this.download}>{u.NAME}{this.props.lessonid}{u.FILE_ID}</li>)
+                    }
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default FileUpload;

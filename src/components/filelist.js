6747ee54dce507f24dfa306ab3efde8cbcc0aff4
Pi-31415 <paingthetko.pi@gmail.com>
2020-09-06 17:18:17 +0630

    .

--- src/components/filelist.js
+++ src/components/filelist.js
@@ -3,20 +3,38 @@ import axios from 'axios';
 
 class FileUpload extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {
+            filelist: ''
+        }
+
+    }
 
 
     submit() {
-       
+
     }
 
     componentDidMount() {
-        alert("Yes");
+
+        var url = "https://zmsedu.com/api/admin/homework/get";
+        axios.post(url,{}, { // receive two parameter endpoint url ,form data 
+        })
+            .then(res => {
+                console.log(res.data);
+            
+                //then edit the Lessons
+                //
+            }).catch(error => {
+                console.log(error);
+            });
     }
 
     render() {
         return (
             <div>
-               asdf {this.props.lessonid}
+                asdf {this.props.lessonid}
             </div>
         )
     }
